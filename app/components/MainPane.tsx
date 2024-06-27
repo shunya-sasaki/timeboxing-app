6c0615d03257aa3d1e1e9aa5f18b0b1fdea5ce84
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2024-06-27 23:41:32 +0900

    Update the layout of panes

--- app/components/MainPane.tsx
+++ app/components/MainPane.tsx
@@ -2,7 +2,7 @@ import { TaskTable } from "./tasktable/TaskTable";
 
 export const MainPane = () => {
   return (
-    <div className="p-4">
+    <div className="p-2">
       <div className="pt-4">
         <TaskTable tableName="AM" />
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -8,21 +8,22 @@ import { MainPane } from "./components/MainPane";
 export default function Home() {
   return (
     <RecoilRoot>
-      <main className="min-w-max h-lvh">
-        <header className="lvw min-w-max h-[5%] z-50">
+      <main className="w-screen min-w-max h-lvh overflow-auto">
+        <header className="fixed top-0 w-screen lvw min-w-max h-10 z-50">
           <Header />
         </header>
-        <div className="w-max h-[92.5%] min-h-[92.5%] z-0">
+        <div className="w-max min-h-[92.5%] mt-10 z-0">
           <div className="flex h-full">
-            <div className="w-auto min-w-32 h-full border-r-2">
+            <div className="fixed z-0 bg-white w-auto min-w-32 h-full border-r-2">
               <SideBar />
             </div>
-            <div className="w-max min-w-auto h-full overflow-scroll">
+            <div className="ml-32 w-max min-w-max h-full">
               <MainPane />
+              <div></div>
             </div>
           </div>
         </div>
-        <footer className="h-[2.5%] z-50">
+        <footer className="fixed bottom-0 w-screen h-6 z-50">
           <Footer />
         </footer>
       </main>
